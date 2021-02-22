4f815543ed68fa5166d3ab162de3066d5fa31f09
Alberto Chávez Pérez <andalusier@pm.me>
2021-02-22 19:48:37 +0100

    Date utils

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,19 +1,20 @@
 function _getDateYYYYMMDD(): string {
     const today = new Date();
-    const DD: string = String(today.getDate()).padStart(2, "0");
-    const MM: string = String(today.getMonth() + 1).padStart(2, "0");
-    const YYYY: number = today.getFullYear();
+    const DD = String(today.getDate()).padStart(2, "0");
+    const MM = String(today.getMonth() + 1).padStart(2, "0");
+    const YYYY = String(today.getFullYear());
     return `${YYYY}-${MM}-${DD}`;
 }
 
 function _getTimeHHMMSS(): string {
     const today = new Date();
-    const HH: string = String(today.getHours()).padStart(2, "0");
-    const MM: string = String(today.getMinutes()).padStart(2, "0");
-    const SS: string = String(today.getSeconds()).padStart(2, "0");
+    const HH = String(today.getHours()).padStart(2, "0");
+    const MM = String(today.getMinutes()).padStart(2, "0");
+    const SS = String(today.getSeconds()).padStart(2, "0");
     return `${HH}:${MM}:${SS}`;
 }
 
+// ToDo: use lib like Luxon
 export function getLocalTimestamp(): string {
-    return `${_getDateYYYYMMDD()} ${_getTimeHHMMSS()}`;
+    return `${_getDateYYYYMMDD()} ${_getTimeHHMMSS()} (+1h)`;
 }
